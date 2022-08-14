4b02fc92c40ab01ee203ed42d98349062b938d20
MoltenPL85 <bugaev.maxim.85@gmail.com>
2022-08-14 11:01:23 +0500

    Initial commit

---
+++ cmd/storage/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/moltenpl85/storage/internal/storage"
+)
+
+func main() {
+	st := storage.NewStorage()
+
+	file, err := st.Upload("test.txt", []byte("Hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	restoredFile, err := st.GetByID(file.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("it is restored", restoredFile)
+}
